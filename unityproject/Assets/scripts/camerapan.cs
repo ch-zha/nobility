5c194cb09e56b2dbe43e387eb4d7c555933a12bf
teires <chizhang95@gmail.com>
2017-06-16 02:37:47 -0700

    sceneskeleton
    camera pan, basic click handling

---
+++ unityproject/Assets/scripts/camerapan.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camerapan : MonoBehaviour {
+
+	//MANUALS
+	public float LEFTBOUND;
+	public float RIGHTBOUND;
+	public Vector3 CAMERA_SPEED = new Vector3(1, 0, 0);
+
+	//GAME OBJECTS/COMPONENTS
+	private Camera cam;
+
+	//STATIC VARS
+	private Vector3 CAMERA_STARTINGPOS;
+
+	//NONSTATIC VARS
+	private float keydir;
+	private Vector3 movement;
+	private float cam_pos;
+
+	/*Init*/
+	void Start () {
+		cam = this.gameObject.GetComponent<Camera> ();
+		CAMERA_STARTINGPOS = cam.transform.position;
+	}
+
+	/*Check for edge of world*/
+	bool hitEdge(float keydir){
+		cam_pos = cam.transform.position.x;
+
+		if (cam_pos <= LEFTBOUND && keydir < 0) {
+			return true;
+		} else if (cam_pos >= RIGHTBOUND && keydir > 0) {
+			return true;
+		}
+		return false;
+	}
+
+	/* Update is called once per frame */
+	void Update () {
+
+		//update movement
+		keydir = Input.GetAxis ("Horizontal");
+		movement = Vector3.Scale(CAMERA_SPEED, new Vector3(keydir, 0, 0));
+	}
+
+	void FixedUpdate () {
+		
+		//check for edge of world
+		if (hitEdge (keydir)) {
+			return;
+		}
+
+		//move camera
+		cam.transform.Translate (movement);
+	}
+}
