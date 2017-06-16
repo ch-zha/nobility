73e8e8ae0bffef896b8d99542ffee7f03a7de4fe
teires <chizhang95@gmail.com>
2017-06-16 05:11:24 -0700

    uiskeleton
    switch between battle & world scenes

---
+++ unityproject/Assets/scripts/battlemanager.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class battlemanager : MonoBehaviour {
+
+	public void endBattle() {
+		UnityEngine.SceneManagement.SceneManager.LoadScene (0);
+	}
+}
